465b60579888385185402cdf5074821bc17ed98f
Jens Hjort Schwee <jensschwee@msn.com>
2015-09-14 09:26:23 +0200

    test

---
+++ ME1/Foundation/BusinessDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Foundation
+{
+    public class BusinessDal
+    {
+        private static string fileLocation = "~/BusinessObj.obj";
+
+        public List<Common.Business> ReadBusiness()
+        {
+            System.Xml.Serialization.XmlSerializer reader =
+                new System.Xml.Serialization.XmlSerializer(typeof(Business));
+            System.IO.StreamReader file = new System.IO.StreamReader(fileLocation);
+            List<Business> businessList = (List<Business>)reader.Deserialize(file);
+            file.Close();
+            return businessList;
+
+        }
+
+        public void WriteBusiness(List<Business> businessList)
+        {
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(Business));
+
+            System.IO.FileStream file = System.IO.File.Create(fileLocation);
+
+            writer.Serialize(file, businessList);
+            file.Close();
+        }
+    }
+}
