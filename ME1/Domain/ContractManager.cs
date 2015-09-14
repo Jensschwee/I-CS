a2991667a918a887971ed5945d5905682f6dc702
Jens Hjort Schwee <jensschwee@msn.com>
2015-09-14 09:45:22 +0200

    fisk

---
+++ ME1/Domain/ContractManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class ContractManager
+    {
+        public void RegisterLeasing()
+        {
+        }
+
+        public void RegisterSale()
+        {
+        }
+
+        public void GetSale()
+        {
+
+        }
+
+        public void GetLeasing()
+        {
+        }
+    }
+}
---
+++ ME1/Domain/CustosmerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Domain
+{
+    public class CustosmerManager
+    {
+        private List<Business> listBusinesses = new List<Business>();
+        private List<Private> listPrivates = new List<Private>();
+
+        public void RegisterBusinessCustomer()
+        {
+        }
+
+        public void RegisterPrivateCustomer()
+        {
+        }
+    }
+}
