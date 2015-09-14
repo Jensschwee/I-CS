857da3ef6aae39a4ee2519ce25e33006a99ae38c
Jens Hjort Schwee <jensschwee@msn.com>
2015-09-14 10:12:53 +0200

    read and write business obj

--- ME1/Domain/CustosmerManager.cs
+++ ME1/Domain/CustosmerManager.cs
@@ -21,14 +21,21 @@ namespace Domain
             listPrivates = dal.ReadPrivate();
         }
 
-        public void RegisterBusinessCustomer(string fax, string companyName, string contractPerson, string cVR, string name, string phone, string personalAddress, string city, string postCode, string country, List<Truck> truckList = null)
+        public void RegisterBusinessCustomer(string fax, string companyName, string contractPerson, string cVR, string name, string phone, string personalAddress, string city, string postCode, string country)
         {
-
+            Business business = new Business(fax,companyName,contractPerson,cVR,name,phone,personalAddress,city,postCode,country);
+            listBusinesses.Add(business);
+            saveBusinessCustomer(listBusinesses);
         }
 
         public void RegisterPrivateCustomer()
         {
 
+        }
+
+        public void saveBusinessCustomer(List<Business> businessObj)
+        {
+            dal.WriteBusiness(businessObj);
         }
     }
 }
