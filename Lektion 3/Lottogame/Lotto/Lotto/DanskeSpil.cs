49dd925d8a593b0aa2f131d11f037329b12475c0
Jens Hjort Schwee <jensschwee@msn.com>
2015-09-29 10:53:00 +0200

    Lektion 3 Lotto

---
+++ Lektion 3/Lottogame/Lotto/Lotto/DanskeSpil.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Lotto
+{
+    /// <summary>
+    /// Simulate Lotto game provider "Danske Spil"
+    /// Replace comment with your code
+    /// </summary>
+    /// <author> Steffen Skov</author>
+    class DanskeSpil
+    {
+        // define a DELEGAT so it can sende Lotto numbers to subscribes
+        public delegate void LottoNumbers(int[] num);
+
+        // Create an EVENT or a delegate instance
+        public event LottoNumbers LottoNumbersEvent;
+
+        public DanskeSpil() { }
+
+        private static List<LottoPlayer> players = new List<LottoPlayer>();
+        static void Main(string[] args)
+        {
+            DanskeSpil ds = new DanskeSpil();
+            LottoPlayer pl = new LottoPlayer(ds);
+            players.Add(pl);
+
+            pl = new LottoPlayer(ds, new[] { 5, 9, 15, 22, 24, 29, 32 });
+            players.Add(pl);
+
+            // Try to create more player with different numbers
+            // When number is set remember to catch exception
+            ds.Draw();
+        }
+
+        /// <summary>
+        /// This method simulate drawing at DanskeSpil
+        /// </summary>
+        public void Draw()
+        {
+            Random n = new Random(DateTime.Now.Millisecond);
+            int[] num = new int[7];
+            for (;;)
+            {
+                for (int i = 0; i < 7; i++)
+                    num[i] = n.Next(1, 36);
+
+                // To increase the probability so only one of numbers will change
+                Console.WriteLine(num[0] + " " + num[1] + " " + num[2] + " " + num[3] + " " + num[4] + " " + num[5] + "\n");
+                if (LottoNumbersEvent != null)
+                {
+                    LottoNumbersEvent(num);
+                    //LottoNumbersEvent(new[] { 5, 9, 15, 22, 24, 29, 32 });
+                }
+                Thread.Sleep(6000);
+                // FIRE the event
+            }
+        }
+    }
+
+}
